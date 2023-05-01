b8ff663928bb7a874dbaa3a50b78aa1e47e47ac3
Xenia Francine Umbal <xeniafrancine.umbal@ad.mynt.xyz>
2023-05-01 21:20:10 +0800

    Added homework #2

---
+++ day 2/PrimeNumber.java
@@ -0,0 +1,26 @@
+class PrimeNumber {
+    public static void main(String[] args) {
+        // call the isPrimeNumberMethod
+        if (isPrimeNumber(4)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+    static boolean isPrimeNumber(int num) {
+        // check if number is less than or equal to 1
+        if (num <= 1) {
+            return false;
+        } 
+
+        // check if number is divisible by other numbers 
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
