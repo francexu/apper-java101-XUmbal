ad99608fcd0855fb1bf41372902dc66c57104104
Xenia Francine Umbal <xeniaumbal@gmail.com>
2023-05-02 10:50:06 +0800

    Add number datatypes

---
+++ day 2/java-101-cycle3/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+        // numeric whole number datatypes: short (16 bits), int (32 bits), long (64 bits)
+        // numeric decimal datatypes: double (64 bits), float (32 bits)
+
+        float discount = 9.25f; // kailangan may f lagi
+        double price = 4.45;
+        int age = 20;
+
+        float ans = discount + age;
+        System.out.println(ans);
+
+        double newAns = discount + price;
+        System.out.println(newAns);
+    }
+}
