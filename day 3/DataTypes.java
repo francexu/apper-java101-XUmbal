ab7da7904caaaf0c036266c53f149ce38891eab7
Xenia Francine Umbal <xeniaumbal@gmail.com>
2023-05-05 09:52:38 +0800

    Add DataTypes.java

---
+++ day 3/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+
+    // Zero value
+
+    static int age;
+    static double salary;
+    static boolean isHappy;
+    static char favoriteLetter;
+
+    public static void main(String[] args) {
+        System.out.println(age); // 0
+        System.out.println(salary); // 0.0
+        System.out.println(isHappy); // false
+        System.out.println(favoriteLetter); // null
+
+        // wrapper
+    }
+}
